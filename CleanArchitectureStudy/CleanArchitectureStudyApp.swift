eea27dedad6870228acf344425abf787d0cfb7a2
trianglejerry <trianglejerry00@gmail.com>
2025-06-26 14:51:30 +0900

    :tada: 프로젝트 파일 추가

---
+++ CleanArchitectureStudy/CleanArchitectureStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureStudyApp.swift
+//  CleanArchitectureStudy
+//
+//  Created by 장은석 on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
