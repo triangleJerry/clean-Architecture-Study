5f5b8382e25d197dc9bb77e8830986f8d16b727e
trianglejerry <trianglejerry00@gmail.com>
2025-08-18 17:43:12 +0900

    User 추가

---
+++ CleanArchitectureStudy/Presentation/View/RamdomUserView.swift
@@ -0,0 +1,18 @@
+//
+//  RamdomUserView.swift
+//  CleanArchitectureStudy
+//
+//  Created by 장은석 on 8/18/25.
+//
+
+import SwiftUI
+
+struct RamdomUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RamdomUserView()
+}
